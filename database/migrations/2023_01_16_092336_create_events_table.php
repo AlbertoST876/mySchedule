ba26ed61a894d239f538ff84ba2888dd20497d4c
AlbertoST876 <albertost876.ast@gmail.com>
2023-01-16 11:22:11 +0100

    Creación de la Base de Datos

---
+++ database/migrations/2023_01_16_092336_create_events_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create("events", function (Blueprint $table) {
+            $table -> id();
+            $table -> foreignId("user_id") -> references("id") -> on("users");
+            $table -> foreignId("category_id") -> references("id") -> on("categories");
+            $table -> string("name", 50);
+            $table -> string("description", 255);
+            $table -> dateTime("date");
+            $table -> timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists("events");
+    }
+};
