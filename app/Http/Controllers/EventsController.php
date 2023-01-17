7fb6b92c255d10acf8aedbbfaf736d692e2cda6e
AlbertoST876 <albertost876.ast@gmail.com>
2023-01-17 11:46:50 +0100

    Cambio controlador de eventos y agregado generación de usuarios

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -2,17 +2,83 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 class EventsController extends Controller
 {
-    public function index() {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
         return view("events.index");
     }
 
-    public function show() {
-        return view("events.show");
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
     }
 
-    public function edit() {
-        return view("events.edit");
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return view("events.show", $id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return view("events.edit", $id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this -> call(
-            CategorySeeder::class
-        );
+        $this -> call([
+            CategorySeeder::class,
+            UserSeeder::class
+        ]);
     }
 }
