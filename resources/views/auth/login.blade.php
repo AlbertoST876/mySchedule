7dc8cc4a416f2845a61a55d198cf234813346d86
AlbertoST876 <albertost876.ast@gmail.com>
2023-02-13 10:19:16 +0100

    Agregado tailwind al login y register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,34 +8,53 @@
 
         <main>
             @if (session("status"))
-                <span>{{ session("status") }}</span>
+                <div class="flex justify-center w-full mx-4 my-4">
+                    <div id="toast-success" class="flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
+                        <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
+                            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+                            <span class="sr-only">Icono de Verificación</span>
+                        </div>
+
+                        <div class="ml-3 text-sm font-normal">{{ session("status") }}</div>
+
+                        <button type="button" class="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-success" aria-label="Close">
+                            <span class="sr-only">Cerrar</span>
+                            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                        </button>
+                    </div>
+                </div>
             @endif
 
-            <form action="" method="post">
+            <form class="mx-12 my-12" action="" method="post">
                 @csrf
 
-                <div>
-                    <label for="email">Email:</label>
-                    <input type="email" name="email" max="50" required>
+                <div class="mb-6">
+                    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="email">Email:</label>
+                    <input class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="email" name="email" max="50" required>
 
                     @error("email")
-                        <span>{{ $message }}</span>                        
+                        <div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+                            <span class="font-medium">{{ $message }}</span>
+                        </div>
                     @enderror
                 </div>
 
-                <div>
-                    <label for="password">Contraseña:</label>
-                    <input type="password" name="password" max="255" required>
+                <div class="mb-6">
+                    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="password">Contraseña:</label>
+                    <input class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="password" name="password" max="255" required>
                 </div>
 
-                <div>
-                    <input type="checkbox" name="remember">
-                    <label for="remember">Recuérdame</label>
+                <div class="flex items-start mb-6">
+                    <div class="flex items-center h-5">
+                        <input class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" type="checkbox" name="remember">
+                    </div>
+
+                    <label class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300" for="remember">Recuérdame</label>
                 </div>
 
-                <div>
-                    <a href="{{ route("register") }}">Registrarse</a>
-                    <input type="submit" name="login" value="Iniciar Sesión">
+                <div class="mb-6">
+                    <input class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="submit" name="login" value="Iniciar Sesión">
+                    <a class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" href="{{ route("register") }}">Registrarse</a>
                 </div>
             </form>
         </main>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,44 +7,50 @@
         @include("layouts.header")
 
         <main>
-            <form action="" method="post">
+            <form class="mx-12 my-12" action="" method="post">
                 @csrf
 
-                <div>
-                    <label for="email">Email:</label>
-                    <input type="email" name="email" value="{{ old("email") }}" max="50" required>
+                <div class="mb-6">
+                    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="email">Email:</label>
+                    <input class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="email" name="email" value="{{ old("email") }}" max="50" required>
 
                     @error("email")
-                        <span>{{ $message }}</span>                        
+                        <div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+                            <span class="font-medium">{{ $message }}</span>
+                        </div>
                     @enderror
                 </div>
 
-                <div>
-                    <label for="name">Nombre de Usuario:</label>
-                    <input type="text" name="name" value="{{ old("name") }}" max="25" required>
+                <div class="mb-6">
+                    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="name">Nombre de Usuario:</label>
+                    <input class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" name="name" value="{{ old("name") }}" max="25" required>
 
                     @error("name")
-                        <span>{{ $message }}</span>                        
+                        <div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+                            <span class="font-medium">{{ $message }}</span>
+                        </div>
                     @enderror
                 </div>
 
-                <div>
-                    <label for="password">Contraseña:</label>
-                    <input type="password" name="password" max="255" required>
+                <div class="mb-6">
+                    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="password">Contraseña:</label>
+                    <input class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="password" name="password" max="255" required>
 
                     @error("password")
-                        <span>{{ $message }}</span>                        
+                        <div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+                            <span class="font-medium">{{ $message }}</span>
+                        </div>
                     @enderror
                 </div>
 
-                <div>
-                    <label for="password_confirmation">Contraseña:</label>
-                    <input type="password" name="password_confirmation" max="255" required>
+                <div class="mb-6">
+                    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="password_confirmation">Contraseña:</label>
+                    <input class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="password" name="password_confirmation" max="255" required>
                 </div>
 
-                <div>
-                    <a href="{{ route("login") }}">Iniciar Sesión</a>
-                    <input type="submit" name="register" value="Registrarse">
+                <div class="mb-6">
+                    <input class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="submit" name="register" value="Registrarse">
+                    <a class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" href="{{ route("login") }}">Iniciar Sesión</a>
                 </div>
             </form>
         </main>
