9af356f40ce9e96ca58b29f757dfa65405aa3d88
AlbertoST876 <albertost876.ast@gmail.com>
2024-01-10 17:59:52 +0100

    Cambiadas las traducciones de messages a app

---
+++ lang/en/app.php
@@ -0,0 +1,129 @@
+<?php
+
+return [
+    // Auth
+        // Login
+        "email" => "Email",
+        "password" => "Password",
+        "rememberme" => "Remember me",
+
+        // Register
+        "username" => "Username",
+        "password_confirm" => "Password confirm",
+        "youMustVerified" => "You must verify your email to continue",
+        "verified" => "Your email has been successfully verified",
+
+        // Settings
+        "account_settings" => "Account Settings",
+        "categories_colors" => "Categories Colors",
+        "profileImg" => "Profile Image",
+        "profileImg_formats" => "Format PNG, JPG or JPEG.",
+        "profileImg_size" => "Max size of 2 Mb.",
+        "profileImg_dimensions" => "Minimum width and height of 128 Pixels and maximum of 2048 Pixels.",
+        "profileImg_recomendation" => "It's recommended that the aspect ratio of the image be 1:1 (eg 512 x 512 Pixels).",
+        "profileImg_delete_confirm" => "Are you sure you want to delete your profile picture?",
+        "change" => "Change",
+        "timeZone" => "Time Zone",
+
+    // Calendar
+        // Week
+        "monday" => "Mon",
+        "thuesday" => "Tues",
+        "wednesday" => "Wed",
+        "thursday" => "Thurs",
+        "friday" => "Fri",
+        "saturday" => "Sat",
+        "sunday" => "Sun",
+
+        // Month
+        "january" => "January",
+        "february" => "February",
+        "march" => "March",
+        "april" => "April",
+        "may" => "May",
+        "june" => "June",
+        "jule" => "Jule",
+        "august" => "August",
+        "september" => "September",
+        "october" => "October",
+        "november" => "November",
+        "december" => "December",
+        "week_number" => "Week Nº",
+
+    // Emails
+        "rememberEmail" => "Remember email",
+        "rememberMessage" => "We remind you that your event is the",
+
+    // Events
+        // Edit
+        "title" => "Title",
+        "description" => "Description",
+        "datetime" => "Date and Time",
+        "remember" => "Remember",
+        "color" => "Color",
+        "return" => "Return",
+
+        // Index
+        "prevEvents" => "Last Events",
+        "nextEvents" => "Next Events",
+        "create_event" => "Create event",
+        "close_create_event" => "Close event create",
+        "create" => "Create",
+
+    // Layouts
+        // Calendar
+            // Nav
+            "day" => "Day",
+            "week" => "Week",
+            "month" => "Month",
+            "year" => "Year",
+            "consult" => "Consult",
+
+        // Events
+            // Event
+            "see" => "See",
+            "edit" => "Edit",
+            "delete" => "Delete",
+            "delete_confirm" => "Are you sure you want to delete the event?",
+
+        // Header
+        "open_user_menu" => "Open user menu",
+        "alt_profileImg" => "Profile image",
+        "settings" => "Settings",
+        "logout" => "Log out",
+        "logout_confirm" => "Are you sure you want to log out?",
+        "open_menu" => "Open menu",
+        "home" => "Home",
+        "calendar" => "Calendar",
+        "events" => "Events",
+
+        // Footer
+        "all_rights_reserved" => "All rights reserverd.",
+        "about_us" => "About Us",
+        "privacy_policy" => "Privacy Policy",
+        "final_user_license" => "Final User License",
+        "contact" => "Contact",
+
+        // Warn
+        "verification_icon" => "Verification icon",
+        "close" => "Close",
+
+    // Index
+    "appName" => "MySchedule",
+    "alt_appIcon" => "Application Icon",
+    "alt_appImg" => "Application Image",
+    "intro" => "Welcome to your new personal schedule stored in the cloud, here you can save your notes, tasks, events, reminders, etc..., whatever you need without having to install any additional application on any of your devices.",
+    "login" => "Log in",
+    "register" => "Register",
+
+    // EventsController
+    "event_created" => "The event was created successfully",
+    "event_edited" => "The event was edited successfully",
+    "event_deleted" => "The event was deleted successfully",
+
+    // UserController
+    "user_registered" => "You have successfully registered, now you must verify your email",
+    "user_logged" => "You are logged in",
+    "user_settings_updated" => "Settings saved successfully",
+    "user_logged_out" => "You have logged out",
+];
